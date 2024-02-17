dea2b14ed89fa31bc85b2e2723e28b010479bfcf
Priyamwada priya <priyamwada8priya@gmail.com>
2024-02-17 23:05:29 +0530

    initial-commit

--- src/reducer/CartReducer.jsx
+++ src/reducer/CartReducer.jsx
@@ -112,9 +112,11 @@ const CartReducer = (state,action) => {
 
   if (action.type === "CART_TOTAL_ITEM") {
       let updatedItemVal = state.cart.reduce((initialVal, curElem) => {
+        
         let { amount } = curElem;
   
         initialVal = initialVal + amount;
+
         return initialVal;
       }, 0);
   
